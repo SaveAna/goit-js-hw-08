fdc90515f09b248a783b967bcc1bb7556a26af6e
Anastasiia Savenok <i.wasber@gmail.com>
2023-03-23 21:46:02 +0200

    add localStorage saves

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onTimeUpdate = function (currentTime) {
+  localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(currentTime.seconds)
+  );
+};
+
+player.on('timeupdate', onTimeUpdate);
+
+const time = localStorage.getItem('videoplayer-current-time');
+//   console.log(time);
+
+player
+  .setCurrentTime(time)
+  .then(function (seconds) {
+    seconds = time;
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
